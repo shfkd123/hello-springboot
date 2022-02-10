3b1b6309b726b4d29d0de39559827047eb70ba94
shfkd123 <shfkd123@gmail.com>
2022-02-10 23:45:15 +0900

    회원 repository, service 생성하고 테스트 코드 작성

---
+++ src/main/java/com/todo/project/hellotodoList/HelloTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.todo.project.hellotodoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/todo/project/hellotodoList/HelloTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.project.hellotodoList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
